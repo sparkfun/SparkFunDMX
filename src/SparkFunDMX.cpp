a29a109a21c58a7a12f9625d16fbf681777ff4f4
Andrew England <andrew.england@sparkfun.local>
2019-07-22 14:43:51 -0600

    forgot to save .cpp

--- src/SparkFunDMX.cpp
+++ src/SparkFunDMX.cpp
@@ -1,14 +1,17 @@
-// - - - - -
-// ESPDMX - A Arduino library for sending and receiving DMX using the builtin serial hardware port.
-// ESPDMX.cpp: Library implementation file
-//
-// Copyright (C) 2015  Rick <ricardogg95@gmail.com>
-// This work is licensed under a GNU style license.
-//
-// Last change: Marcel Seerig <https://github.com/mseerig>
-//
-// Documentation and samples are available at https://github.com/Rickgg/ESP-Dmx
-// - - - - -
+/******************************************************************************
+SparkFunDMX.h
+Arduino Library for the SparkFun ESP32 LED to DMX Shield
+Andy England @ SparkFun Electronics
+7/22/2019
+
+Development environment specifics:
+Arduino IDE 1.6.4
+
+This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+Please review the LICENSE.md file included with this example. If you have any questions 
+or concerns with licensing, please contact techsupport@sparkfun.com.
+Distributed as-is; no warranty is given.
+******************************************************************************/
 
 /* ----- LIBRARIES ----- */
 #include <Arduino.h>
